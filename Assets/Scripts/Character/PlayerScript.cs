fa33d78d67600187863eba81089ab2d9f974cf6a
kangms5296 <rlfhfhdhk@naver.com>
2019-04-12 02:08:07 +0900

    캐릭터 이동 추가, 간단한 맵 배치 및 여러 Effect 추가

--- Assets/Scripts/Character/PlayerScript.cs
+++ Assets/Scripts/Character/PlayerScript.cs
@@ -2,18 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerScript : MonoBehaviour
+public class PlayerScript : MonoBehaviour, CharacterScript
 {
+    public float moveSpeed = 100f;
+    public float rotateSpeed = 0.15f;
 
-    // Start is called before the first frame update
+    Rigidbody rigid;
+    
+    float h, v;
+
     void Start()
     {
-        
+        rigid = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
     }
+    void FixedUpdate()
+    {
+        // 캐릭터 이동 및 회전
+        Move();
+    }
+
+
+    // ===================================================== public function ============================================================
+
+    public void Attack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Damaged()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Move()
+    {
+        // 캐릭터 이동
+        Vector3 movement = new Vector3(h, 0f, v);
+        movement = movement * moveSpeed * Time.deltaTime;
+        rigid.velocity = movement;
+
+        // 캐릭터 회전
+        Quaternion rotate = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), rotateSpeed);
+        rigid.rotation = rotate;
+    }
+
+    public void Rotate()
+    {
+    }
+
+    // ===================================================== private function ============================================================
+
 }
---
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // 전체 객체의 속도 제어를 위한 스피드 값
+    public static float allSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
